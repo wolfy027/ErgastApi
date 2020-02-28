1f515c59468ac6c9d2c8255813367fda4f8460ce
wolfy027 <jomon027@gmail.com>
2020-02-28 22:52:47 +0530

    spring boot template added

---
+++ src/main/java/api/ergast/wrapper/ErgastApiApplication.java
@@ -0,0 +1,13 @@
+package api.ergast.wrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErgastApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/ergast/wrapper/ErgastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ergast.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErgastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
